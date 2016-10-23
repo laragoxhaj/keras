5720792895334af415af6769e2fce9dd2f847b5b
LaraGoxhaj <laragoxhaj@gmail.com>
2016-10-23 00:48:06 -0400

    MASE

--- keras/metrics.py
+++ keras/metrics.py
@@ -33,7 +33,7 @@ def mean_absolute_scaled_error(y_true, y_pred):
     '''Non-seasonal
     '''
     e_forecast = y_true - y_pred
-    return K.mean( K.abs(e_forecast) / mean_absolute_error(y_true[1:len(y_true)], y_true[0:-1]) )
+    return K.mean( K.abs(e_forecast) / mean_absolute_error(y_true[1:y_true.shape[0]], y_true[0:-1]) )
 
 
 def mean_squared_logarithmic_error(y_true, y_pred):
